6b88a0d86ffaf4e20536b9655e73261062a54399
Eric Bäckhage <eric.backhage@gmail.com>
2023-06-23 10:56:59 +0200

    Added Prime number generation

---
+++ Applications/Primes.py
@@ -0,0 +1,31 @@
+import heapq
+
+
+def primes_less_than(n):
+    is_prime = [False] * 2 + [True] * (n - 1)
+
+    for x in range(2, int(n**0.5)):
+        if is_prime[x]:
+            for i in range(x**2, n, x):
+                is_prime[i] = False
+
+    for i in range(n):
+        if is_prime[i]:
+            yield i
+
+
+def primes():
+    composite = []
+    i = 2
+
+    while True:
+        # Note that composite[0][0] is the min element of the heap
+        if composite and i == composite[0][0]:
+            while composite[0][0] == i:
+                multiple, p = heapq.heappop(composite)
+                heapq.heappush(composite, [multiple + p, p])
+        else:
+            heapq.heappush(composite, [i * i, i])
+            yield i
+
+        i += 1
---
+++ Test/testPrimes.py
@@ -0,0 +1,44 @@
+import unittest
+
+from Applications import Primes
+
+
+class TestPrimes(unittest.TestCase):
+    def test_generate_primes_less_than(self):
+        primes = Primes.primes_less_than(100)
+        self.assertListEqual(
+            [
+                2,
+                3,
+                5,
+                7,
+                11,
+                13,
+                17,
+                19,
+                23,
+                29,
+                31,
+                37,
+                41,
+                43,
+                47,
+                53,
+                59,
+                61,
+                67,
+                71,
+                73,
+                79,
+                83,
+                89,
+                97,
+            ],
+            list(primes),
+        )
+
+    def test_primes_stream(self):
+        all_primes = Primes.primes()
+        first_ten_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+        for i in range(len(first_ten_primes)):
+            self.assertEqual(first_ten_primes[i], next(all_primes))
