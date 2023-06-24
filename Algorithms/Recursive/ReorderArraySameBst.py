334e7ab2dab3a79a6baee6ca145d86cebdbf948e
Eric Bäckhage <eric.backhage@gmail.com>
2023-06-24 20:49:37 +0200

    Add explanatory text to Number of ways to reorder array

--- Algorithms/Recursive/ReorderArraySameBst.py
+++ Algorithms/Recursive/ReorderArraySameBst.py
@@ -13,8 +13,14 @@ def number_of_ways(nums):
         left_nodes = [n for n in nums if n < root]
         right_nodes = [n for n in nums if n > root]
 
-        return (
-            dfs(left_nodes) * dfs(right_nodes) * math.comb(m - 1, len(left_nodes)) % mod
-        )
+        # Recursively call dfs on the left nodes and the right nodes to calculate
+        # the ways to reorder them.
+        # Also, the result needs to be multiplied by the binomial coefficient, which
+        # represents the number of ways to reorder the elements without altering the
+        # relative order of the nodes.
+        binomial = math.comb(
+            m - 1, len(left_nodes)
+        )  # m - 1 since the root must stay in place
+        return dfs(left_nodes) * dfs(right_nodes) * binomial % mod
 
     return (dfs(nums) - 1) % mod
