319f064b939115df1ce301344cb4b6663dd56cc2
Eric Bäckhage <eric.backhage@gmail.com>
2023-07-14 11:14:47 +0200

    Remove unneccesary code

--- Test/testSmallerElementsToTheRight.py
+++ Test/testSmallerElementsToTheRight.py
@@ -8,7 +8,3 @@ class TestSmallerElementsToTheRight(unittest.TestCase):
         input = [3, 4, 9, 6, 1]
         expected_result = [1, 1, 2, 1, 0]
         self.assertListEqual(smaller_count(input), expected_result)
-
-
-if __name__ == "__main__":
-    unittest.main()
